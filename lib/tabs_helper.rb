1bbc0e962f0305cc3153c2f22c91f5abc4b53e65
Daniel Lopes <danielvlopes@gmail.com>
2009-05-24 17:05:29 -0300

    Change code to accept blocks, remake readme.

--- lib/tabs_helper.rb
+++ lib/tabs_helper.rb
@@ -1,6 +1,6 @@
 # TabsForRails
 module TabsHelper
-  
+
   def self.included(base) # base is the class that included the module
     base.extend(ClassMethods)
   end
@@ -25,9 +25,9 @@ module TabsHelper
   #     <li><a href="/accounts">Account</a></li>
   #     <li><a href="/users">Users</a></li>
   #     <li><a href="/" class="current">Dashboard</a></li>
-  #     <li><a href="/projects">Projects</a></li>    
-  #   </ul>  
-  
+  #     <li><a href="/projects">Projects</a></li>
+  #   </ul>
+
   module ClassMethods
     def current_tab(name, options = {})
       before_filter(options) do |controller|
@@ -39,6 +39,7 @@ module TabsHelper
   module Helpers
     module ViewHelpers
       class Tab
+
         def initialize(context)
           @context = context
         end
@@ -47,19 +48,41 @@ module TabsHelper
           @context.instance_variable_get('@current_tab')
         end
 
-        def method_missing(tab, name, options = {}, html_options = {})
-          html_options[:class].nil? ? html_options[:class] = 'current' : html_options[:class] << 'current' if tab.to_s == current_tab.to_s
-          "<li>#{@context.link_to(name, options, html_options)}</li>"
+        def current_tab?(tab)
+          current_tab.to_s == tab.to_s
         end
+        
+        def create_tab(tab, content)
+          css_class = "class='current'" if tab.to_s == current_tab.to_s
+          "<li #{css_class}>#{content}</li>"
+        end
+        
+        def method_missing(tab, *args, &block)
+          if block_given?
+            options      = args.first || {}
+            html_options = args.second || {}
+            link_content = @context.capture(&block)
+            link         = @context.link_to (link_content, options, html_options)
+            
+            @context.concat create_tab(tab, link)  
+          else
+            name         = args.first
+            options      = args.second || {}
+            html_options = args.third || {}
+            link         = @context.link_to (name, options, html_options)
+            
+            create_tab(tab, link)
+          end
+        end
+
       end
 
       def tabs(options={}, &block)
         raise ArgumentError, "Missing block" unless block_given?
 
-        concat("<ul class='tabs #{options[:class]}'>")
-        yield(Tab.new(self))
+        concat("<ul class='#{options[:class]}'>")
+        yield(Tab.new self )
         concat('</ul>')
-
       end
 
     end
