3eebd712fb49d3142fe287ccad8d81e92698d30f
Daniel Lopes <danielvlopes@gmail.com>
2010-03-06 13:32:31 -0300

    removing unnecessary files and major refactoring in source code

--- lib/tabs_helper/view.rb
+++ lib/tabs_helper/view.rb
@@ -4,47 +4,26 @@ module TabsHelper
 
       def initialize(context)
         @context = context
-      end
-
-      def current_tab
-        @context.instance_variable_get('@current_tab')
+        @current_tab = @context.instance_variable_get('@current_tab')
       end
 
       def current_tab?(tab)
-        current_tab.to_s == tab.to_s
-      end
-
-      def create_tab(tab, content)
-        css_class = "class='current'" if tab.to_s == current_tab.to_s
-        "<li #{css_class}>#{content}</li>"
+        @current_tab.to_s == tab.to_s
       end
 
       def method_missing(tab, *args, &block)
-        if block_given?
-          options      = args.first || {}
-          html_options = args.second || {}
-          link_content = @context.capture(&block)
-          link         = @context.link_to(link_content, options, html_options)
-
-          create_tab(tab, link)
-        else
-          name         = args.first
-          options      = args.second || {}
-          html_options = args.third || {}
-          link         = @context.link_to(name, options, html_options)
-
-          create_tab(tab, link)
-        end
+        css_class = "current" if current_tab?(tab)
+        @context.content_tag :li, @context.link_to(*args, &block), :class=>css_class
       end
 
     end
 
-    def tabs(options={}, &block)
+    def tabs(options={})
       raise ArgumentError, "Missing block" unless block_given?
-      concat("<ul#{tag_options(options)}>")
+      concat tag(:ul, options, true)
       yield Tab.new(self)
-      concat('</ul>')
+      concat "</ul>"
     end
-
+    
   end
 end
